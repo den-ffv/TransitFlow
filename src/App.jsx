1f0899b361849efac7abec6a52d85dd99c91701a
Bohdan C <chokhlenkoo@gmail.com>
2024-01-28 23:09:21 +0200

    the beginning of the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.scss"
+
+function App() {
+  return (
+    <>
+      <h1>TransitFlow</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, impedit?
+      </p>
+    </>
+  );
+}
+
+export default App;
